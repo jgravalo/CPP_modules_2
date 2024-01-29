e95a2fb08fa4a30615296ff3c700947e39f4c6d0
Jesús <jesusgrawork361@gmail.com>
2024-01-29 07:45:21 +0100

    ex02

---
+++ 07/ex02/inc/Array.hpp
@@ -0,0 +1,24 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include<iostream>
+
+class Array
+{
+	private:
+		/* data */
+	public:
+		Array();
+		~Array();
+};
+
+#endif
+
+Array::Array()
+{
+}
+
+Array::~Array()
+{
+}
+
