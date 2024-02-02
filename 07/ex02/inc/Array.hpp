5a66eba99c144762d137dd2c436b2ab356b6b023
jgravalo <jesusgrawork361@gmail.com>
2024-02-02 15:39:44 +0100

    07

--- 07/ex02/inc/Array.hpp
+++ 07/ex02/inc/Array.hpp
@@ -3,13 +3,18 @@
 
 #include<iostream>
 
+template <typename T>
 class Array
 {
 	private:
-		/* data */
+		T	*T;
 	public:
 		Array();
+		Array(unsigned int n);
+		Array(class Array& Copy);
+		Array&	operator=(class Array& Copy);
 		~Array();
+		Array&	operator[](int n);
 };
 
 #endif
@@ -18,7 +23,28 @@ Array::Array()
 {
 }
 
+Array::Array(unsigned int n)
+{
+	T = n;
+}
+
+Array::Array(class Array& Copy)
+{
+	(void)Copy;
+}
+
+Array&	Array::operator=(class Array& Copy)
+{
+	(void)Copy;
+	return (*this);
+}
+
 Array::~Array()
 {
+}
+
+Array&	Array::operator[](int n)
+{
+	return (T);
 }
 
--- 07/ex02/src/main.cpp
+++ 07/ex02/src/main.cpp
@@ -1,6 +1,8 @@
-
+#include<Array.hpp>
 
 int main()
 {
+	int * a = new int(5);
+	std::cout << *a << std::endl;
 	return 0;
 }
